01ce76ca2b07665df76dc7e86604e2372e46607c
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2021-01-12 22:38:46 +0100

    added closing modal after logged in and alert to show logged in

--- src/js/auth.js
+++ src/js/auth.js
@@ -20,6 +20,12 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 
+const loginBtn = document.querySelector('.login-btn');
+const logoutBtn = document.querySelector('.logout-btn');
+const signupBtn = document.querySelector('.signup-btn');
+const navSpinner = document.querySelector('.render');
+const closeLogInBtn = document.querySelector('.closeBtn');
+
 const signForm = document.querySelector('#signForm');
 signForm.addEventListener('submit', e => {
 	e.preventDefault();
@@ -34,10 +40,6 @@ signForm.addEventListener('submit', e => {
 });
 
 // logout
-const loginBtn = document.querySelector('.login-btn');
-const logoutBtn = document.querySelector('.logout-btn');
-const signupBtn = document.querySelector('.signup-btn');
-const navSpinner = document.querySelector('.render');
 
 const setupLoginBtns = user => {
 	if (user) {
@@ -53,37 +55,40 @@ const setupLoginBtns = user => {
 
 logoutBtn.addEventListener('click', e => {
 	e.preventDefault();
-	auth.signOut();
+	auth.signOut().then(() => {
+		logForm['logSubmit'].classList.remove('d-none');
+		document.querySelector('#loggedInAlert').classList.add('d-none');
+	});
 });
 
 // log in
-const myModal = document.getElementById('loginModal');
 
 const logForm = document.getElementById('logForm');
 logForm.addEventListener('submit', e => {
 	e.preventDefault();
-	console.log('clicked');
-	// myModal.modal('hide');
 	const email = logForm['emailToLogIn'].value;
 	const password = logForm['passwordToLogIn'].value;
-	console.log(email, password);
+	// console.log(email, password);
 
-	auth.signInWithEmailAndPassword(email, password);
+	auth
+		.signInWithEmailAndPassword(email, password)
+		.then(() => {
+			document.querySelector('#loggedInAlert').classList.remove('d-none');
+			logForm['logSubmit'].classList.add('d-none');
+		})
+		.then(() => closeLogInBtn.click())
+		.then(() => logForm.reset());
 });
-
-const toRender = document.querySelector('.render');
 
 const renderBtns = () => {
 	logoutBtn.classList.remove('invisible');
 	loginBtn.classList.remove('invisible');
 	signupBtn.classList.remove('invisible');
 	navSpinner.classList.add('invisible');
-	console.log('render btns');
 };
 
 // listen to user status
 auth.onAuthStateChanged(user => {
-	console.log('change', user);
 	renderBtns();
 	if (user) {
 		setupLoginBtns(user);
